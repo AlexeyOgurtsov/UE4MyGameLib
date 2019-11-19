5c7f2d8017c6c4c01e2b8bcc2b74183d920ec752
alexey_eng <alexey_eng@mail.ru>
2019-11-19 17:18:38 +0500

    SpriteTrackUtilsLib: Code and simple test

--- Source/MyGameLib/SplineTrack/SplineTrackGeneratorLib.cpp
+++ Source/MyGameLib/SplineTrack/SplineTrackGeneratorLib.cpp
@@ -3,32 +3,73 @@
 
 #include "Components/SplineComponent.h"
 #include "Components/SplineMeshComponent.h"
+//#include "Engine/EngineTypes.h" // FAttachmentTransformRules
 
 /**
 * @TODO
 */
 
+bool USplineTrackGeneratorLib::ResetUniformSplineTrack
+(
+	USplineComponent* Spline,
+	const FSplineTrackSegment& SegmentTemplate,
+	EMyObjectCreationFlags CreationFlags
+)
+{
+	checkf(Spline, TEXT("When calling \"%s\" passed spline component pointer must be valid NON-null pointer"), TEXT(__FUNCTION__));
+	AActor* const Actor = Spline->GetOwner();
+	checkf(Actor, TEXT("When calling \"%s\" owner of the passed spline component must be valid NON-null pointer"), TEXT(__FUNCTION__));
+	DestroyAllSplineMeshComponents(Actor);
+	return CreateUniformSplineTrack(Spline, SegmentTemplate, CreationFlags);
+}
+
+bool USplineTrackGeneratorLib::ResetUniformSplineTrack_Validate
+(
+	USplineComponent* Spline,
+	const FSplineTrackSegment& SegmentTemplate,
+	EMyObjectCreationFlags CreationFlags
+)
+{
+	return CreateUniformSplineTrack_Validate(Spline, SegmentTemplate, CreationFlags);
+}
+
+void USplineTrackGeneratorLib::DestroyAllSplineMeshComponents(AActor* Actor)
+{
+	checkf(Actor, TEXT("When calling \"%s\" passed actor must be valid NON-null pointer"), TEXT(__FUNCTION__));
+	TArray<USplineMeshComponent*> SplineMeshes;
+	Actor->GetComponents<USplineMeshComponent>(SplineMeshes, /*bIncludeFromChildActors*/false);
+	for(USplineMeshComponent* SplineMesh : SplineMeshes)
+	{
+		SplineMesh->DestroyComponent(/*bPromoteChildren*/false);
+	}
+}
+
+bool USplineTrackGeneratorLib::DestroyAllSplineMeshComponents_Validate(AActor* Actor)
+{
+	return Actor != nullptr;
+}
+
 USplineMeshComponent* USplineTrackGeneratorLib::CreateAttachedSplineSegmentMesh
 (
 	USplineComponent* const Spline, int32 const SegmentIndex,
 	const FSplineTrackSegment& SegmentData,
-	USceneComponent* const ParentComponent,
-	bool bDynamicObject,
+	EMyObjectCreationFlags CreationFlags,
 	FName SubobjectName
 )
 {
 	checkf(Spline, TEXT("When calling \"%s\" passed spline component pointer must be valid NON-null pointer"), TEXT(__FUNCTION__));
 	checkf(SegmentIndex >=0, TEXT("When calling \"%s\" segment index must be NON-negative"), TEXT(__FUNCTION__));
-	int32 const NumSegments = Spline->IsClosedLoop() ? (Spline->GetNumberOfSplinePoints()) : (Spline->GetNumberOfSplinePoints() - 1);
+	// @TODO: Refactor as a separate util
+	int32 const NumSegments = GetNumberOfSplineTrackSegments(Spline);
 	checkf(SegmentIndex < NumSegments, TEXT("When calling \"%s\" segment index must be less than number of segments"), TEXT(__FUNCTION__));
-	checkf(ParentComponent, TEXT("When calling \"%s\" passed parent component pointer must be valid NON-null pointer"), TEXT(__FUNCTION__));
-	AActor* const OwnerActor = ParentComponent->GetOwner();
-	checkf(OwnerActor, TEXT("When calling \"%s\" owner actor of the passed parent component pointer must be valid NON-null pointer"), TEXT(__FUNCTION__));
+	AActor* const OwnerActor = Spline->GetOwner();
+	checkf(OwnerActor, TEXT("When calling \"%s\" owner actor of the passed spline component must be valid NON-null pointer"), TEXT(__FUNCTION__));
 
 	int32 const StartIndex = SegmentIndex;
 	int32 const EndIndex = (SegmentIndex < Spline->GetNumberOfSplinePoints()) ? (SegmentIndex + 1) : 0;
 
 	USplineMeshComponent* SplineMesh = nullptr;
+	bool const bDynamicObject = (CreationFlags & EMyObjectCreationFlags::Dynamic) != EMyObjectCreationFlags::None;
 	if(bDynamicObject)
 	{
 		SplineMesh = NewObject<USplineMeshComponent>(OwnerActor, SubobjectName);
@@ -56,9 +97,78 @@ USplineMeshComponent* USplineTrackGeneratorLib::CreateAttachedSplineSegmentMesh
 		float const EndRoll = Spline->GetRollAtSplinePoint(EndIndex, SplineCoordSpace);
 		SplineMesh->SetEndRoll(EndRoll, false);
 	}
+	
+	{	
 
-	SplineMesh->SetupAttachment(ParentComponent);
+		USceneComponent* const ParentComponent = Spline;
+		if(bDynamicObject)
+		{
+			FAttachmentTransformRules Rules { EAttachmentRule::KeepRelative, /*bWeldSimulatedBodies*/false };
+			bool bAttached = SplineMesh->AttachToComponent(ParentComponent, Rules);
+			M_LOG_ERROR_IF( ! bAttached, TEXT("USplineMesh::AttachToComponent failed while calling \"%s\""), TEXT(__FUNCTION__) );
+			if( ! bAttached )
+			{
+				return nullptr;
+			}
+		}
+		else
+		{
+			SplineMesh->SetupAttachment(ParentComponent);
+		}
+	}
 	SplineMesh->UpdateMesh();
 
 	return SplineMesh;
+}
+
+bool USplineTrackGeneratorLib::CreateAttachedSplineSegmentMesh_Validate
+(
+	USplineComponent* Spline, int32 SegmentIndex,
+	const FSplineTrackSegment& SegmentData,
+	EMyObjectCreationFlags CreationFlags,
+	FName SubobjectName
+)
+{
+	return (Spline != nullptr);
+}
+
+bool USplineTrackGeneratorLib::CreateUniformSplineTrack
+(
+	USplineComponent* Spline,
+	const FSplineTrackSegment& SegmentTemplate,
+	EMyObjectCreationFlags CreationFlags
+)
+{
+	checkf(Spline, TEXT("When calling \"%s\" passed spline component pointer must be valid NON-null pointer"), TEXT(__FUNCTION__));
+	// @TODO: Make real parent component
+	int32 const NumSegments = GetNumberOfSplineTrackSegments(Spline);
+	for(int32 SegmentIndex = 0; SegmentIndex < NumSegments; SegmentIndex++)
+	{
+		USplineMeshComponent* SplineMesh = CreateAttachedSplineSegmentMesh (Spline, SegmentIndex, SegmentTemplate, CreationFlags);
+		if(SplineMesh == nullptr)
+		{
+			return false;
+		}
+	}
+	return true;
+}
+bool USplineTrackGeneratorLib::CreateUniformSplineTrack_Validate
+(
+	USplineComponent* Spline,
+	const FSplineTrackSegment& SegmentTemplate,
+	EMyObjectCreationFlags CreationFlags
+)
+{
+	return (Spline != nullptr);
+}
+
+int32 USplineTrackGeneratorLib::GetNumberOfSplineTrackSegments(USplineComponent* Spline)
+{
+	checkf(Spline, TEXT("When calling \"%s\" passed spline component pointer must be valid NON-null pointer"), TEXT(__FUNCTION__));
+	return Spline->IsClosedLoop() ? (Spline->GetNumberOfSplinePoints()) : (Spline->GetNumberOfSplinePoints() - 1);
+}
+
+bool USplineTrackGeneratorLib::GetNumberOfSplineTrackSegments_Validate(USplineComponent* Spline)
+{
+	return Spline != nullptr;
 }
--- Source/MyGameLib/SplineTrack/SplineTrackGeneratorLib.h
+++ Source/MyGameLib/SplineTrack/SplineTrackGeneratorLib.h
@@ -2,6 +2,7 @@
 
 #include "Kismet/BlueprintFunctionLibrary.h"
 #include "SplineTrackTypes.h"
+#include "Util/Core/Object/MyObjectCreationTypes.h"
 #include "SplineTrackGeneratorLib.generated.h"
 
 class USplineComponent;
@@ -14,8 +15,48 @@ class USplineTrackGeneratorLib : public UBlueprintFunctionLibrary
 	GENERATED_BODY()
 
 public:
+	/**
+	* Like CreateUniformSplineTrack, but removes all spline mesh components before adding any new.
+	*
+	* @see: CreateUniformSplineTrack
+	*/
+	UFUNCTION(BlueprintCallable, Category=SplineTrackGeneratorLib, Meta=(WithValidation="true"))
+	static bool ResetUniformSplineTrack
+	(
+		USplineComponent* Spline,
+		const FSplineTrackSegment& SegmentTemplate,
+		EMyObjectCreationFlags CreationFlags = EMyObjectCreationFlags::Dynamic
+	);
+	static bool ResetUniformSplineTrack_Validate
+	(
+		USplineComponent* Spline,
+		const FSplineTrackSegment& SegmentTemplate,
+		EMyObjectCreationFlags CreationFlags
+	);
+
+	/**
+	* CreateUniformSplineTrack
+	*
+	* @return: true if the track was created without errors
+	*/
+	UFUNCTION(BlueprintCallable, Category=SplineTrackGeneratorLib, Meta=(WithValidation="true"))
+	static bool CreateUniformSplineTrack
+	(
+		USplineComponent* Spline,
+		const FSplineTrackSegment& SegmentTemplate,
+		EMyObjectCreationFlags CreationFlags = EMyObjectCreationFlags::Dynamic
+	);
+	static bool CreateUniformSplineTrack_Validate
+	(
+		USplineComponent* Spline,
+		const FSplineTrackSegment& SegmentTemplate,
+		EMyObjectCreationFlags CreationFlags
+	);
+
 	/** 
 	* CreateAttachedSplineSegmentMesh
+	*
+	* Spline mesh will component be attached automatically to the spline component.
 	*
 	* @param Spline            Spline component to get spline data from
 	* (must be valid NON-null pointer)
@@ -23,23 +64,38 @@ public:
 	* of the segment in question, starting from ZERO.
 	* It's possible to pass the last spline point index
 	* if the spline is looped.
-	* @param ParentComponent   Parent component to attach to
-	* (must be valid NON-null pointer)
+	* @param ParentComponent: must be valid pointer!
 	* @param SubobjectName    Name of the spline mesh component 
 	* subobject that is to be created
-	* @param bDynamicObject Must be true if called NOT from constructor, false otherwise.
-	* If NAME_None is passed, the name is to be generated automatically.
+	* If NAME_None is passed, then the name is to be generated automatically.
+	* @param CreationFlags    Flags that determine how the object is to be created
+	* Warning: if not within construction script or constructor is called,
+	* Dynamic flag must be passed!
 	*
-	* @returns always returns valid pointer (checked with checkf)
+	* @returns returns pointer to the created spline mesh, or nullptr, if there was an error
+	* while attaching the spline segment mesh.
 	*/
-	UFUNCTION(BlueprintCallable, Category=SplineTrackGeneratorLib, Meta=(UnsafeDuringActorConstruction="false"))
+	UFUNCTION(BlueprintCallable, Category=SplineTrackGeneratorLib, Meta=(WithValidation="true"))
 	static USplineMeshComponent* CreateAttachedSplineSegmentMesh
 	(
 		USplineComponent* Spline, int32 SegmentIndex,
 		const FSplineTrackSegment& SegmentData,
-	 	USceneComponent* ParentComponent,
-		bool bDynamicObject,
+		EMyObjectCreationFlags CreationFlags = EMyObjectCreationFlags::Dynamic,
 		FName SubobjectName = NAME_None
 	);
+	static bool CreateAttachedSplineSegmentMesh_Validate
+	(
+		USplineComponent* Spline, int32 SegmentIndex,
+		const FSplineTrackSegment& SegmentData,
+		EMyObjectCreationFlags CreationFlags,
+		FName SubobjectName
+	);
 	
+	UFUNCTION(BlueprintCallable, Category=SplineTrackGeneratorLib, Meta=(WithValidation="true"))
+	static void DestroyAllSplineMeshComponents(AActor* Actor);
+	static bool DestroyAllSplineMeshComponents_Validate(AActor* Actor);
+
+	UFUNCTION(BlueprintPure, Category=SplineTrackGeneratorLib, Meta=(WithValidation="true"))
+	static int32 GetNumberOfSplineTrackSegments(USplineComponent* Spline);
+	static bool GetNumberOfSplineTrackSegments_Validate(USplineComponent* Spline);
 };
